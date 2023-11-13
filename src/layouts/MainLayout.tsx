b8bd57f9d55338c38f887d6fb26d7dc7225a0c12
Jorge Vazquez <jorge@totema.co>
2023-11-13 14:55:34 +0100

    more fixes

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -10,9 +10,9 @@ const MainLayout = (props: MainLayoutProps) => {
     const { children } = props;
 
     return (
-        <div className={`${styles.MainLayout} container mx-auto px-4`}>
-            <h1 className={`${styles.Header} text-3xl font-bold my-4 text-center`}>LiveLink</h1>
-            <div className={`${styles.MainContent}`}>{children}</div>
+        <div className={styles.MainLayout}>
+            <h1 className={styles.Header}>LiveLink</h1>
+            <div className={styles.MainContent}>{children}</div>
         </div>
     )
 };
